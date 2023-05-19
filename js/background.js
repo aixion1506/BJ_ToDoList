e6e9c162c44add86536320e35eab4a7197a997a0
aixion1506 <aixion1506@gmail.com>
2023-05-19 15:50:31 +0900

    patch/background

--- js/background.js
+++ js/background.js
@@ -1,10 +1,17 @@
+// 이미지 파일 이름을 담은 배열
 const images = ['cat1.jpg', 'dog1.jpg'];
+// 배열에서 랜덤으로 이미지 선택
 const chosenImage = images[Math.floor(Math.random() * images.length)];
+// 선택된 이미지로 배경 이미지 URL 생성
 const bgImage = `url(img/${chosenImage})`;
 
-function randomBackground() {
+// 배경화면 랜덤 함수
+const randomBackground = () => {
+  // body 요소의 배경 이미지로 설정
   document.body.style.backgroundImage = bgImage;
-}
+};
 
+// 초기에 한 번 배경 이미지 설정
 randomBackground();
+// 1초마다 배경 이미지 업데이트
 setInterval(randomBackground, 1000);
